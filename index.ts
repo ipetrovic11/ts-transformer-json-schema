9a539c140888437f6a65fdf2664b4dc15d256f86
Dan Kuida <dan@kuida.org>
2019-08-16 14:44:02 +0200

    Develop (#1)
    * sync
    
    * sync
    
    * linting and better code tooling
    
    * travvis
    
    * travvis
    
    * travvis

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as transformer from "./transformer";
-
-export * from "./predefined";
+import * as transform from './transformer';
+export const transformer = transform;
+export * from './predefined';
 export declare function schema<T extends object>(additional?: boolean): any;
--- test/validator.spec.ts
+++ test/validator.spec.ts
@@ -1,32 +1,29 @@
-"use strict";
+import * as Validator from 'fastest-validator';
 
-const Validator = require('fastest-validator');
-const v = new Validator();
+const v = new (Validator as any)();
 
-import { schema } from '../index';
-import { IExternal } from './interfaces';
-import { IEmail, IDate, IForbidden, IUrl, IUUID } from '../predefined';
+import { schema, IEmail, IUUID } from '../index';
 
-describe("Test validator directly with json schema transformer", () => {
+describe('Test validator directly with json schema transformer', () => {
 
-	describe("Union optional", () => {
-		it("Should succeed with email", () => {
+	describe('Union optional', () => {
+		it('Should succeed with email', () => {
 			interface IUnion {
 				union?: IEmail | IUUID;
 			}
 
-			expect(v.validate({ union: "john@acme.com"}, schema<IUnion>())).toBe(true);
+			expect(v.validate({ union: 'john@acme.com'}, schema<IUnion>())).toBe(true);
 		});
 
-		it("Should succeed with email", () => {
+		it('Should succeed with email', () => {
 			interface IUnion {
 				union?: IEmail | IUUID;
 			}
 
-			expect(v.validate({union: "083c5eb4-87cc-42f4-9b9d-691af310a423"}, schema<IUnion>())).toBe(true);
+			expect(v.validate({union: '083c5eb4-87cc-42f4-9b9d-691af310a423'}, schema<IUnion>())).toBe(true);
 		});
 
-		it("Should succeed empty", () => {
+		it('Should succeed empty', () => {
 			interface IUnion {
 				union?: IEmail | IUUID;
 			}
@@ -34,75 +31,75 @@ describe("Test validator directly with json schema transformer", () => {
 			expect(v.validate({}, schema<IUnion>())).toBe(true);
 		});
 
-		it("Should fail with number", () => {
+		it('Should fail with number', () => {
 			interface IUnion {
 				union?: IEmail | IUUID;
 			}
 
-			expect(v.validate({union: 1}, schema<IUnion>())).toBeInstanceOf(Array)
+			expect(v.validate({union: 1}, schema<IUnion>())).toBeInstanceOf(Array);
 		});
 
-		it("Should fail with object", () => {
+		it('Should fail with object', () => {
 			interface IUnion {
 				union?: IEmail | IUUID;
 			}
 
-			expect(v.validate({union: { email: "john@acme.com"}}, schema<IUnion>())).toBeInstanceOf(Array);
+			expect(v.validate({union: { email: 'john@acme.com'}}, schema<IUnion>())).toBeInstanceOf(Array);
 		});
 	});
 
-	describe("Index interface", () => {
-		it("Should succeed with object with string prop", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+	describe('Index interface', () => {
+		it('Should succeed with object with string prop', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
-			expect(v.validate({index: {a: "string" }}, schema<IIndex>())).toBe(true);
+			expect(v.validate({index: {a: 'string' }}, schema<IIndex>())).toBe(true);
 		});
 
-		it("Should succeed with object with number prop", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+		it('Should succeed with object with number prop', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
 			expect(v.validate({index: {a: 1 }}, schema<IIndex>())).toBe(true);
 		});
 
-		it("Should succeed with object with object prop", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+		it('Should succeed with object with object prop', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
 			expect(v.validate({index: {a: {} }}, schema<IIndex>())).toBe(true);
 		});
 
-		it("Should fail with null", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+		it('Should fail with null', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
 			expect(v.validate({index: null }, schema<IIndex>())).toBeInstanceOf(Array);
 		});
 
-		it("Should fail with string", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+		it('Should fail with string', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
-			expect(v.validate({index: "string"}, schema<IIndex>())).toBeInstanceOf(Array);
+			expect(v.validate({index: 'string'}, schema<IIndex>())).toBeInstanceOf(Array);
 		});
 
-		it("Should fail with array", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+		it('Should fail with array', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
 			expect(v.validate({index: [] }, schema<IIndex>())).toBeInstanceOf(Array);
 		});
 
-		it("Should fail without index prop", () => {
-			interface IIndex { 
-				index: { [group: string]: string[] }
+		it('Should fail without index prop', () => {
+			interface IIndex {
+				index: { [group: string]: string[] };
 			}
 
 			expect(v.validate({a: {} }, schema<IIndex>())).toBeInstanceOf(Array);
