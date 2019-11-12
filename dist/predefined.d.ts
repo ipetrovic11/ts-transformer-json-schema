0fc5526c896f08c31c392957cc68796feeb52ce1
Darryl <dswoods@gmail.com>
2019-11-11 18:23:09 -0800

    Fix compile issues

--- dist/predefined.d.ts
+++
@@ -1,5 +0,0 @@
-export interface IDate extends Date{}
-export interface IEmail extends String{}
-export interface IForbidden{}
-export interface IUrl extends String{}
-export interface IUUID extends String{}
